ce7483ec7541bce9743cae8b0161fa64b8507e2c
Mike Kelly <pioto@pioto.org>
2014-06-25 03:42:30 -0400

    use a more-correct line type

--- tanks.cpp
+++ tanks.cpp
@@ -156,7 +156,7 @@ draw_tanks() {
     // turret
     glPushMatrix();
     glRotatef(RAD_TO_DEG(angle), 0, 0, 1);
-    glBegin(GL_LINE_STRIP);
+    glBegin(GL_LINES);
     glVertex3f(0, 0, 0);
     glVertex3f(0.1, 0, 0);
     glEnd();
